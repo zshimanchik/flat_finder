023543078ef7fa876474f35db21cb9928dfd306a
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2017-03-25 13:06:36 +0300

    removed pytelegrambotapi dependency

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -1,10 +1,17 @@
-import telebot
+import requests
 
 
 class TelegramNotifier:
+    _BASE_URL = 'https://api.telegram.org/bot{token}/{method}'
+
     def __init__(self, bot_token, chat_id):
-        self.bot = telebot.TeleBot(bot_token)
-        self.chat_id = chat_id
+        self._send_message_url = self._BASE_URL.format(token=bot_token,
+                                                       method='sendMessage')
+        self._chat_id = chat_id
 
     def notify(self, text):
-        self.bot.send_message(self.chat_id, text)
+        data = {
+            'chat_id': self._chat_id,
+            'text': text,
+        }
+        requests.post(self._send_message_url, json=data)
